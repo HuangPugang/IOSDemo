c723020dc2d69b21c07927f4519e355b6063e5bf
huangpu <huangpg@59store.com>
2015-12-17 10:11:05 +0800

    Initial Commit

---
+++ IOSDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  IOSDemo
+//
+//  Created by Paul on 15/12/17.
+//  Copyright (c) 2015年 Paul. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ IOSDemo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  IOSDemo
+//
+//  Created by Paul on 15/12/17.
+//  Copyright (c) 2015年 Paul. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
